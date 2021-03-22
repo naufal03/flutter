b7e2fd6bf26faf2a746aa7258af38f9c0240b5bc
Naufalarkan03 <naufalarkan134@gmial.com>
2021-03-22 17:42:55 +0700

    first commit

---
+++ lib/Screens/Home/KandidatPertama.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'Visi1.dart';
+
+class KandidatPertama extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.purpleAccent,
+          title: Text('Calon Ketua Osis'),
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              scale: 4,
+              image: AssetImage(
+                'assets/icons/wlp.jpg',
+              ),
+            ),
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: SafeArea(
+              child: Column(
+                children: <Widget>[
+                  new SizedBox(
+                    height: 150,
+                    child: Card(
+                      color: Colors.purpleAccent,
+                      elevation: 3.0,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+                      child: Row(
+                        children: <Widget>[
+                          new Padding(padding: EdgeInsets.all(10.0)),
+                          new Image.asset(
+                            'assets/images/Arkan.jpg',
+                            width: 140,
+                            height: 120,
+                          ),
+                          new Text('Naufal Arkan '),
+                          new RaisedButton(
+                            color: Colors.purple,
+                            child: Text(
+                              'More',
+                              style: TextStyle(
+                                  color: Colors.purpleAccent,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (contex) => Visi1()));
+                            }
+
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  new SizedBox(
+                    height: 150,
+                    child: Card(
+                      color: Colors.purpleAccent,
+                      elevation: 3.0,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+                      child: Row(
+                        children: <Widget>[
+                          new Padding(padding: EdgeInsets.all(10.0)),
+                          new Image.asset(
+                            'assets/images/Galuh.jpg',
+                            width: 140,
+                            height: 120,
+                          ),
+                          new Text('Galuh S           '),
+                          new RaisedButton(
+                            color: Colors.purple,
+                            child: Text(
+                              'More',
+                              style: TextStyle(
+                                  color: Colors.purpleAccent,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            onPressed: () {},
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  new SizedBox(
+                    height: 150,
+                    child: Card(
+                      color: Colors.purpleAccent,
+                      elevation: 3.0,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+                      child: Row(
+                        children: <Widget>[
+                          new Padding(padding: EdgeInsets.all(10.0)),
+                          new Image.asset(
+                            'assets/images/Sahid.jpg',
+                            width: 140,
+                            height: 120,
+                          ),
+                          new Text('Sahid F           '),
+                          new RaisedButton(
+                            color: Colors.purple,
+                            child: Text(
+                              'More',
+                              style: TextStyle(
+                                  color: Colors.purpleAccent,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            onPressed: () {},
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Screens/Home/Visi1.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Visi1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+        child: Padding(padding: EdgeInsets.all(20.0)),
+          ),
+        ),
+      );
+  }
+}
